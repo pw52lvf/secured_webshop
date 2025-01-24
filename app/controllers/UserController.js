ea4a07d336affa072f964ba52ede3e88ca27a74c
pw52lvf <pw52lvf@eduvaud.ch>
2025-01-24 15:29:48 +0100

    fin séquence 2

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,5 +1,24 @@
 module.exports = {
     get: (req, res) => {
-        res.send("User: Sarah Test");
+        res.send(`
+        <!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Authentification</title>
+    </head>
+    <body>
+        <h1>Authentification</h1>
+        <form method="get" action="">
+            <p>Donnez-moi votre argent</p>
+            <label>Nom d'utilisateur</label> : <input type="email" name="Username" id="Username"/>
+            <br/><br/>
+            <label>Mot de passe</label> : <input type="text" name="Password" id="Password"/>
+            <br/><br/>
+            <button>Submit</button>
+        </form>
+    </body>
+</html>
+                `);
     }
 };
